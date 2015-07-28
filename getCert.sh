6d14b42bb0649f752557beef6d063359506136fd
Matt Faulkner <matthew.faulkner@genesys.com>
2015-07-28 13:19:12 +0100

    OpenSSL Script to get remote certificate

---
+++ getCert.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Taken from https://www.madboa.com/geek/openssl/#cert-retrieve
+# usage: retrieve-cert.sh remote.host.name [port]
+#
+REMHOST=$1
+REMPORT=${2:-443}
+
+echo |\
+openssl s_client -connect ${REMHOST}:${REMPORT} 2>&1 |\
+sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'
